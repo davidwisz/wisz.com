b994d39975068fdcd60a999ac9a4575cd64c6b65
DAVID WISZ <david@wisz.com>
2020-07-22 18:17:41 -0700

    added scroll animation using intersect observer

---
+++ assets/js/scroll-animation.js
@@ -0,0 +1,25 @@
+let callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('seen');
+            observer.unobserve(entry.target);
+        }
+    });
+};
+
+let options = {
+    rootMargin: '0px',
+    threshold: .13
+}
+
+let observer = new IntersectionObserver(callback, options);
+
+let targets = document.querySelectorAll('.scroll-in');
+
+targets.forEach(target => {
+    observer.observe(target);
+});
+
+
